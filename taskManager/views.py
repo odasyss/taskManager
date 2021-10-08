0f42f8a291eeb41364aea79a2e394d5b994ca99b
Odasys <odasyss@gmail.com>
2021-10-07 19:46:14 -0700

    register update

--- taskManager/views.py
+++ taskManager/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.contrib.auth.forms import UserCreationForm
@@ -20,12 +20,23 @@ class RegisterPage(FormView):
     redirect_authenticated_user = True
     success_url = reverse_lazy('tasks')
 
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
 
 
 class CustomLoginView(LoginView):
     template_name = 'taskManager/user_login.html'
     fields = '__all__'
-    # redirect_authenticated_user = True
+    redirect_authenticated_user = True
 
     def get_success_url(self):
         return reverse_lazy('tasks')
