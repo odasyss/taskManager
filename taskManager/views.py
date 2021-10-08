be418cb7798719a1eec3176874594c22a5fc226c
Odasys <odasyss@gmail.com>
2021-10-07 17:48:19 -0700

    initial register

--- taskManager/views.py
+++ taskManager/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import FormView
+from .models import Task
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+class RegisterPage(FormView):
+    template_name = 'taskManager/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+
+
+class CustomLoginView(LoginView):
+    template_name = 'taskManager/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
