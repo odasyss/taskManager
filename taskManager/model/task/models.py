dc17705c1febb086802b241d86612c037f02cbd3
Odasys <odasyss@gmail.com>
2021-10-21 18:23:34 -0700

    update 10/21

--- taskManager/model/task/models.py
+++ taskManager/model/task/models.py
@@ -4,16 +4,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Task(models.Model):
-    STATUS_TODO = 1
-    STATUS_IN_PROGRESS = 2
-    STATUS_TESTING = 3
-    STATUS_DONE = 4
-
     STATUS_CHOICES = (
-        (STATUS_TODO, ('Not Started')),
-        (STATUS_IN_PROGRESS, ('In Progress')),
-        (STATUS_TESTING, ('Testing')),
-        (STATUS_DONE, ('Done')),
+        ('todo', 'Todo'),
+    ('progress', 'Progress'),
+    ('review', 'Review'),
+    ('done', 'Done'),
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
@@ -22,7 +17,7 @@ class Task(models.Model):
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)   
     sprint = models.CharField(max_length=200, default="")
-    status = models.SmallIntegerField(choices=STATUS_CHOICES, default=STATUS_TODO)
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES)
     subtask = models.ForeignKey('self',on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
--- taskManager/view/task/views.py
+++ taskManager/view/task/views.py
@@ -14,10 +14,6 @@ from taskManager.model.task.models import Task
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
-    # def get_context_data(self, **kwargs):
-        # context = super().get_context_data(**kwargs)
-        # context['tasks'] = context['tasks'].filter(user=self.request.user)
-        # context['count'] = context['tasks'].filter(complete=False).count()
 
 
 class TaskDetail(LoginRequiredMixin, DetailView):
